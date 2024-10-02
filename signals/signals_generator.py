2bc476869068d3b238698c4300a2ad3b0c5a9ab9
ghiaccionove <ant.cito@gmail.com>
2024-10-02 10:34:51 +0200

    aggiunti i return dei segnali e la funzione per fetchare i simboli

--- signals/signals_generator.py
+++ signals/signals_generator.py
@@ -5,13 +5,18 @@ import pandas as pd
 def rsi_signal(data, overbought=70, oversell=30):
     data = indicators.rsi(data)
     latest_rsi = data['rsi'].iloc[-1]
+    signal_oversell = False
+    signal_overbought = False
     if latest_rsi > overbought:
         print('rsi ipercomprato: ', latest_rsi)
+        signal_overbought = True
+
     elif latest_rsi < oversell:
         print('rsi ipervenduto: ', latest_rsi)
+        signal_oversell = True
     else:
         print('rsi: ', latest_rsi)
-    #return latest_rsi
+    return signal_overbought, signal_oversell
     
 
 def volatility_signal(data, period=60, threshold=0.013):
@@ -20,7 +25,19 @@ def volatility_signal(data, period=60, threshold=0.013):
     latest_volatility = data[volatility_column].iloc[-1]
     if latest_volatility > threshold:
         print('volatility is over the threshold: ', latest_volatility)
+        signal_breakout = True
     else:
-        print('volatility: ', latest_volatility)
+        signal_breakout = False
+    return signal_breakout
 
     
+def parabolic_trend(data):
+    data = indicators.parabolic_sar(data)
+    latest_sar = data['sar'].iloc[-1]
+    if data['close'].iloc[-1] >  latest_sar:
+        print('Price is up parabolic SAR')
+        signal_uptrend = True
+    else:
+        print('Price is down parabolic SAR')
+        signal_uptrend = False
+    return signal_uptrend
--- utils/data_fetcher.py
+++ utils/data_fetcher.py
@@ -4,3 +4,8 @@ def fetch_market_data(exchange, symbol, timeframe='1m', limit=1000):
     data = pd.DataFrame(exchange.ccxt.fetch_ohlcv(symbol, timeframe=timeframe, limit=limit),
                         columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
     return data
+
+def fetch_symbols(exchange, type='swap'):
+    markets = pd.DataFrame(exchange.ccxt.fetch_markets())
+    swap_symbols = markets.loc[(markets['type'] == type)&(markets['quoteId'] == 'USDT'), 'symbol']
+    return swap_symbols
