419ff08e7785bdd299b63f8b3dbfaeb81c30c2b8
ghiaccionove <ant.cito@gmail.com>
2024-10-03 19:13:57 +0200

    aggiunte costanti signal_types e bozza basilare di modulo strategy

---
+++ signals/signal_types.py
@@ -0,0 +1,11 @@
+class Signal:
+    BUY = 'BUY'
+    SELL = 'SELL'
+    HOLD = 'HOLD'
+    OVERBOUGHT = 'OVERBOUGHT'
+    OVERSOLD = 'OVERSOLD'
+    UP = 'UP'
+    DOWN = 'DOWN'
+    HIGH_VOLATILITY = 'HIGH_VOLATILITY'
+    LOW_VOLATILITY = 'LOW_VOLATILITY'
+
--- signals/signals_generator.py
+++ signals/signals_generator.py
@@ -1,23 +1,21 @@
 from signals import indicators
+from signals.signal_types import Signal
 import pandas as pd
 
 
-def rsi_signal(data, overbought=70, oversell=30):
+def rsi_signal(data, overbought=70, oversold=30):
     data = indicators.rsi(data)
     latest_rsi = data['rsi'].iloc[-1]
-    signal_oversell = False
-    signal_overbought = False
     if latest_rsi > overbought:
         print('rsi ipercomprato: ', latest_rsi)
-        signal_overbought = True
+        return Signal.OVERBOUGHT
 
-    elif latest_rsi < oversell:
+    elif latest_rsi < oversold:
         print('rsi ipervenduto: ', latest_rsi)
-        signal_oversell = True
+        return Signal.OVERSOLD
     else:
         print('rsi: ', latest_rsi)
-    return signal_overbought, signal_oversell
-    
+
 
 def volatility_signal(data, period=60, threshold=0.013):
     data = indicators.volatility(data, period=period)
@@ -25,19 +23,18 @@ def volatility_signal(data, period=60, threshold=0.013):
     latest_volatility = data[volatility_column].iloc[-1]
     if latest_volatility > threshold:
         print('volatility is over the threshold: ', latest_volatility)
-        signal_breakout = True
+        return Signal.HIGH_VOLATILITY
     else:
-        signal_breakout = False
-    return signal_breakout
-
+        return Signal.LOW_VOLATILITY
     
+
 def parabolic_trend(data):
     data = indicators.parabolic_sar(data)
     latest_sar = data['sar'].iloc[-1]
-    if data['close'].iloc[-1] >  latest_sar:
-        print('Price is up parabolic SAR')
-        signal_uptrend = True
+    if data['close'].iloc[-1] >=  latest_sar:
+        print('Price is up parabolic SAR', latest_sar)
+        return Signal.UP
     else:
-        print('Price is down parabolic SAR')
-        signal_uptrend = False
-    return signal_uptrend
+        print('Price is down parabolic SAR', latest_sar)
+        return Signal.DOWN
+
